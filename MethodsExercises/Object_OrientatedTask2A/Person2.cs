7e756256ef71886542764415db0b9174c3b89811
Dylan <30002799@g.toiohomai.ac.nz>
2018-08-02 12:32:31 +1200

    Progress.

--- MethodsExercises/Object_OrientatedTask2A/Person2.cs
+++ MethodsExercises/Object_OrientatedTask2A/Person2.cs
@@ -13,6 +13,11 @@ namespace Object_OrientatedTask2A
         int payRate;
         int tax;
         int accountNumber;
+        string occupation;
+        string workHours;
+        string relationshipStatus;
+        string noOfChildren;
+        string country;
         public string Fname
         {
             get
@@ -68,13 +73,74 @@ namespace Object_OrientatedTask2A
                 accountNumber = value;
             }
         }
-        public Person2(string _Fname, string _Lname, int _PayRate, int _Tax, int _AccountNumber)
+        public string Occupation
+        {
+            get
+            {
+                return occupation;
+            }
+            set
+            {
+                occupation = value;
+            }
+        }
+        public string WorkHours
+        {
+            get
+            {
+                return workHours;
+            }
+            set
+            {
+                workHours = value;
+            }
+        }
+        public string RelationshipStatus
+        {
+            get
+            {
+                return relationshipStatus;
+            }
+            set
+            {
+                relationshipStatus = value;
+            }
+        }
+        public string NoOfChildren
+        {
+            get
+            {
+                return noOfChildren;
+            }
+            set
+            {
+                noOfChildren = value;
+            }
+        }
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+            }
+        }
+        
+        public Person2(string _Fname, string _Lname, int _PayRate, int _Tax, int _AccountNumber, string _Occupation, string _WorkHours, string _RelationshipStatus, string _NoOfChildren, string _Country)
         {
             fName = _Fname;
             lName = _Lname;
             payRate = _PayRate;
             tax = _Tax;
             accountNumber = AccountNumber;
+            occupation = _Occupation;
+            workHours = _WorkHours;
+            relationshipStatus = _RelationshipStatus;
+            noOfChildren = _NoOfChildren;
+            country = _Country;
         }
     }
 }
--- MethodsExercises/Object_OrientatedTask2A/Program.cs
+++ MethodsExercises/Object_OrientatedTask2A/Program.cs
@@ -10,7 +10,23 @@ namespace Object_OrientatedTask2A
     {
         static void Main(string[] args)
         {
-            Person2 employee1 = new Person2("John", "Smith", 20, 15, 10);
+            Person2 employee1 = new Person2("John", "Smith", 20, 15, 10, "Builder", "9am - 5pm", "Married", "4 children", "New Zealand");
+            List<Person2> List2 = new List<Person2>
+            {
+                employee1
+            };
+            foreach(Person2 employee in List2)
+            {
+                Console.WriteLine(employee1.Fname);
+                Console.WriteLine(employee1.Lname);
+                Console.WriteLine(employee1.PayRate);
+                Console.WriteLine(employee1.Tax);
+                Console.WriteLine(employee1.AccountNumber);
+                Console.WriteLine(employee1.Occupation);
+                Console.WriteLine(employee1.WorkHours);
+                Console.WriteLine(employee1.)
+                Console.WriteLine(employee1.Country);
+            }
         }
     }
 }
